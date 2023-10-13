dc2e87de988472378f03fcd8150a0711b02aec48
Tanish-Eagle <ta.shrivastava@gmail.com>
2023-10-14 02:59:17 +0530

    Features of the game are now complete. All the bugs have been fixed.

--- snakes-and-ladder.js
+++ snakes-and-ladder.js
@@ -1,13 +1,13 @@
 const startButton = document.getElementById("startButton");
-const rules = document.getElementById("rules");
 const rollButton = document.getElementById("rollButton");
-const message = document.getElementById("message");
+const rules = document.getElementById("rules");
+const playerMessage = document.getElementById("playerMessage");
+const computerMessage = document.getElementById("computerMessage");
 
-let playerPosition = 1; // starting position for the player.
+let playerPosition = 1; // Starting position for the player.
 let computerPosition = 1; // Starting position for the computer.
 
 const snakes = {
-
     16: 6,
     48: 26,
     52: 29,
@@ -20,58 +20,64 @@ const snakes = {
     98: 78,
 };
 
-// function to roll the dice.
+// Function to roll the dice.
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1; // Roll a 6-sided die
 }
 
-
+// Function to play a turn for the player.
 // Function to play a turn for the player.
 function playPlayerTurn() {
-    function playerRoll() {
-        const roll = rollDice();
-        message.textContent = "Player rolled a " + roll;
-        if (roll === 6) {
-            message.textContent += "Player rolled a 6! Roll again!";
-            setTimeout(playerRoll, 1000);
-        }
-        else {
-            playerPosition += roll;
+    const roll = rollDice();
+    playerMessage.textContent = "Player rolled a " + roll;
+
+    if (roll === 6) {
+        playerMessage.textContent += " Player rolled a 6! Roll again!";
+        setTimeout(playPlayerTurn, 1000);
+    } else {
+        const newPosition = playerPosition + roll;
+        if (newPosition <= 100) {
+            playerPosition = newPosition;
             if (snakes[playerPosition]) {
                 playerPosition = snakes[playerPosition];
-                message.textContent = "You landed on a snake's head. You are not at the position " + playerPosition;
-            }
-            else {
-                message.textContent = "Player is at position " + playerPosition;
+                playerMessage.textContent = "You landed on a snake's head. You are now at position " + playerPosition;
+            } else {
+                playerMessage.textContent = "Player is at position " + playerPosition;
             }
             checkWin();
-            if (gameOver && playerPosition !== 100) {
+            if (!gameOver && playerPosition !== 100) {
                 setTimeout(playComputerTurn, 1000);
             }
+        } else {
+            // Player can't move beyond 100, wait for the next turn.
+            playerMessage.textContent = "Player is at position " + playerPosition;
         }
     }
-    playerRoll();
 }
 
 // Function to play a turn for the computer.
-
+// Function to play a turn for the computer.
+// Function to play a turn for the computer.
 function playComputerTurn() {
     function computerRoll() {
         const roll = rollDice();
-        message.textContent = "Computer rolled a " + roll;
+        computerMessage.textContent = "Computer rolled a " + roll;
 
         if (roll === 6) {
-            message.textContent += "Computer rolled a 6! Roll again!";
+            computerMessage.textContent += " Computer rolled a 6! Roll again!";
             setTimeout(computerRoll, 1000); // Delay the next roll for 1 second
         } else {
-            computerPosition += roll;
-            if (snakes[computerPosition]) {
-                computerPosition = snakes[computerPosition];
-                message.textContent = "Oops! Computer landed on a snake's head. Computer is now at position " + computerPosition;
-            } else {
-                message.textContent = "Computer is at position " + computerPosition;
+            const newPosition = computerPosition + roll;
+            if (newPosition <= 100) {
+                computerPosition = newPosition;
+                if (snakes[computerPosition]) {
+                    computerPosition = snakes[computerPosition];
+                    computerMessage.textContent = "Oops! Computer landed on a snake's head. Computer is now at position " + computerPosition;
+                } else {
+                    computerMessage.textContent = "Computer is at position " + computerPosition;
+                }
             }
-            checkWin();
+            checkWin(); // Check for a win condition after the computer's turn
         }
     }
 
@@ -81,33 +87,35 @@ function playComputerTurn() {
 // Function to check if the game is won.
 let gameOver = false;
 function checkWin() {
-
     if (playerPosition === 100) {
-        message.textContent = "Congratulations! You've won!";
+        playerMessage.textContent = "Congratulations! You've won!";
         gameOver = true;
-        rollButton.disabled = true;
+        rollButton.style.display = "none";
         showStartButtonAndRules();
-
-    }
-    else if (computerPosition === 100) {
-        message.textContent = "The computer has won the game.";
+    } else if (computerPosition === 100) {
+        computerMessage.textContent = "The computer has won the game.";
         gameOver = true;
-        rollButton.disabled = true;
+        rollButton.style.display = "none";
         showStartButtonAndRules();
     }
 }
 
-// event listener to begin the game.
+// Event listener to begin the game.
 startButton.addEventListener("click", () => {
     startGame();
 });
 
-// Functions to start the game;
+// Event listener for the "Roll dice button".
 
+rollButton.addEventListener("click", () => {
+    playPlayerTurn();
+});
+
+// Function to start the game.
 function startGame() {
     startButton.style.display = "none";
     rules.style.display = "none";
-
+    rollButton.style.display = "block";
     playPlayerTurn();
 }
 
@@ -115,5 +123,4 @@ function startGame() {
 function showStartButtonAndRules() {
     startButton.style.display = "block";
     rules.style.display = "block";
-
 }
